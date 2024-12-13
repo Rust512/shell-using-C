9be12b1826c326d097a68cf6a5235ee9e799c480
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2024-12-13 15:08:37 +0530

    remove trailing newline character from input

--- app/main.c
+++ app/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int get_string_length(char* str);
+
 int main() {
     // Uncomment this block to pass the first stage
     printf("$ ");
@@ -9,9 +11,27 @@ int main() {
     char input[100];
     fgets(input, 100, stdin);
 
+    // remove the trailing '\n' character from input string.
+    input[get_string_length(input) - 1] = '\0';
+
     // The last character in the command is a '\n', we must remove this character.
     printf("%s: not found\n", input);
     fflush(stdout);
 
     return 0;
+}
+
+int get_string_length(char* str) {
+    if (str == NULL) {
+        return 0;
+    }
+
+    int length = 0;
+
+    while (*str != '\0') {
+        length++;
+        str++;
+    }
+
+    return length;
 }
