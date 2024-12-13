734d7ef774c286719f95cf2d4ac77529e8aa89ab
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2024-12-13 15:00:21 +0530

    handle an invalid command

--- app/main.c
+++ app/main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-  fflush(stdout);
+    // Uncomment this block to pass the first stage
+    printf("$ ");
+    fflush(stdout);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  return 0;
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+
+    // The last character in the command is a '\n', we must remove this character.
+    input[(int)strlen(input) - 1] = '\0';
+
+    // An invalid command:
+    char invalid_command[16] = "invalid_command";
+    if (strcmp(input, invalid_command) == 0) {
+        printf("%s: not found\n", input);
+        fflush(stdout);
+    }
+
+    return 0;
 }
