7473a07e469260b7ad0ebe773b9d15fa7826af2f
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2024-12-13 15:43:38 +0530

    introduce an infinite loop

--- app/main.c
+++ app/main.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
-int get_string_length(char* str);
+#define EXIT "exit"
+
+int get_string_length(char *str);
 
 int main() {
-    // Uncomment this block to pass the first stage
-    printf("$ ");
-    fflush(stdout);
+    while (1) {
+        // Uncomment this block to pass the first stage
+        printf("$ ");
+        fflush(stdout);
 
-    // Wait for user input
-    char input[100];
-    fgets(input, 100, stdin);
+        // Wait for user input
+        char input[100];
+        fgets(input, 100, stdin);
 
-    // remove the trailing '\n' character from input string.
-    input[get_string_length(input) - 1] = '\0';
+        // remove the trailing '\n' character from input string.
+        input[get_string_length(input) - 1] = '\0';
 
-    // The last character in the command is a '\n', we must remove this character.
-    printf("%s: not found\n", input);
-    fflush(stdout);
+        // The last character in the command is a '\n', we must remove this character.
+        printf("%s: not found\n", input);
+        fflush(stdout);
+    }
 
     return 0;
 }
 
-int get_string_length(char* str) {
+int get_string_length(char *str) {
     if (str == NULL) {
         return 0;
     }
