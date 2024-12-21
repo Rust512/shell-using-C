86f2e95b33129579b7e8421b60045cc65c7ed776
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2024-12-22 00:07:28 +0530

    re-write the code in a more elegant way

--- app/main.c
+++ app/main.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
-#include <dirent.h>
-#include <sys/stat.h>
+#include <assert.h>
 
-#define EXIT "exit"
-#define ECHO "echo"
-#define TYPE "type"
-
-#define NOT_FOUND "NOT_FOUND"
-#define ECHO_PATH "echo $PATH"
 #define BUFFER_SIZE 2048
 
-typedef enum repl_state {
-    CONTINUE,
-    BREAK
-} repl_state;
+static const char *EXIT = "exit";
+static const char *ECHO = "echo";
+static const char *TYPE = "type";
+
+typedef struct command {
+    char *name;
+    char *all_args;
+    size_t arg_count;
+    char **args;
+} Command;
+
+typedef enum command_type {
+    INTERNAL,
+    EXTERNAL,
+    UNKNOWN
+} CommandType;
+
+typedef enum command_behavior {
+    TERMINATE,
+    EXECUTE,
+    BACKGROUND
+} CommandBehavior;
+
+typedef CommandBehavior (execute_command)(const Command *);
 
 char *ltrim(char *);
 
@@ -24,54 +38,69 @@ char *rtrim(char *);
 
 char *trim(char *);
 
-char *get_command_name(char *);
+char *get_command_name(const char *);
 
-repl_state execution_sequence(char *);
+char *get_all_args(char *);
 
-void echo_command(char *);
+size_t get_number_of_args(const char *);
 
-void type_command(char *);
+void initialize_args_array(char **, const char *, size_t);
 
-void not_found(char *);
+Command parse_command(const char *);
 
-int is_built_in(const char *);
+void free_command(const Command *);
 
-char *is_external(const char *);
+execute_command *factory(const Command *);
 
-char *get_path_variable();
+CommandType get_command_type(const Command *);
 
-char *get_command_type(char *);
+CommandBehavior execute_generic_command(const Command *);
 
-int get_number_of_paths(const char *);
+CommandBehavior execute_inbuilt_command(const Command *);
 
-void segregate_paths(char **, char *);
+CommandBehavior unknown(const Command *);
 
-char *find_command(char **, int, const char *);
+CommandBehavior type_command(const Command *);
 
-char *find_in_directory(char *, const char *);
+CommandBehavior exit_command(__attribute__((unused)) const Command *);
+
+CommandBehavior echo_command(const Command *);
 
 int main() {
     while (1) {
-        // Uncomment this block to pass the first stage
         printf("$ ");
         fflush(stdout);
 
-        // Wait for user input
-        char *input = (char *) calloc(100, sizeof(char));
-        fgets(input, 100, stdin);
+        char *input = (char *) calloc(BUFFER_SIZE, sizeof(char));
+        fgets(input, BUFFER_SIZE, stdin);
 
-        // trim the command from left and right for any whitespace characters.
-        char *command = trim(input);
-        repl_state state = execution_sequence(command);
+        const char *command = trim(input);
 
-        if (state == BREAK) {
+        if (!strlen(command)) {
+            continue;
+        }
+
+        Command parsed = parse_command(command);
+        CommandBehavior behavior = execute_generic_command(&parsed);
+
+        if (behavior == TERMINATE) {
             break;
         }
 
-        fflush(stdout);
-        free(input);
-    }
+        switch (behavior) {
+            case EXECUTE:
+            case BACKGROUND:
+                free_command(&parsed);
+                free(input);
+                continue;
+            default:
+                break;
+        }
 
+        free_command(&parsed);
+        free(input);
+        fflush(stdout);
+    }
     return 0;
 }
 
@@ -93,165 +122,183 @@ char *trim(char *str) {
     return ltrim(rtrim(str));
 }
 
-char *get_command_name(char *command) {
+char *get_command_name(const char *input) {
+    assert(input != NULL);
     size_t index = 0;
-    while (command[index] != ' ' && command[index] != '\0') {
-        index++;
-    }
-    return strndup(command, index);
+    while (input[index] != ' ' && input[index]) index++;
+    return strndup(input, index);
 }
 
-repl_state execution_sequence(char *command) {
-    if (strlen(command) == 0) {
-        return CONTINUE;
-    }
+char *get_all_args(char *command) {
+    assert(command != NULL);
     char *command_name = get_command_name(command);
 
-    if (strcmp(command_name, ECHO) == 0) {
-        echo_command(command);
-        return CONTINUE;
-    } else if (strcmp(command_name, EXIT) == 0) {
-        return BREAK;
-    } else if (strcmp(command_name, TYPE) == 0) {
-        type_command(command);
-        return CONTINUE;
+    size_t index = strlen(command_name);
+    assert(command[index] == ' ' || command[index] == '\0');
+
+    command = command + index;
+
+    free(command_name);
+    return ltrim(command);
+}
+
+size_t get_number_of_args(const char *command) {
+    assert(command != NULL);
+    char *dup_command = strdup(command);
+    const char *all_args = get_all_args(dup_command);
+
+    size_t total_args_size = strlen(all_args);
+    if (total_args_size == 0) {
+        return 0;
+    }
+
+    bool is_space = false;
+    size_t count = 1;
+
+    for (int index = 0; index < total_args_size; index++) {
+        if (!is_space && all_args[index] == ' ') {
+            is_space = true;
+        }
+
+        if (is_space && all_args[index] != ' ') {
+            is_space = false;
+            count++;
+        }
+    }
+
+    free(dup_command);
+    return count;
+}
+
+void initialize_args_array(char **args, const char *all_args, size_t args_count) {
+    assert(args != NULL);
+    assert(all_args != NULL);
+    size_t start_index = 0;
+    size_t arg_index = 0;
+    bool is_space = false;
+    size_t all_args_size = strlen(all_args);
+    size_t index = 0;
+
+    while (index < all_args_size && arg_index < args_count) {
+        if (!is_space && all_args[index] == ' ') {
+            is_space = true;
+            args[arg_index++] = strndup(all_args + start_index, index - start_index);
+        }
+
+        if (is_space && all_args[index] != ' ') {
+            is_space = false;
+            start_index = index;
+        }
+        index++;
+    }
+
+    args[arg_index] = strdup(all_args + start_index);
+}
+
+Command parse_command(const char *input) {
+    assert(input != NULL);
+
+    char *command_name = get_command_name(input);
+    size_t number_of_args = get_number_of_args(input);
+    if (number_of_args == 0) {
+        Command command = {
+                command_name,
+                "",
+                0,
+                NULL
+        };
+        return command;
+    }
+
+    char *dup_input = strdup(input);
+    char *all_args = get_all_args(dup_input);
+
+    char *args[number_of_args];
+    initialize_args_array(args, all_args, number_of_args);
+    Command command = {
+            command_name,
+            all_args,
+            number_of_args,
+            args
+    };
+
+    return command;
+}
+
+void free_command(const Command *command) {
+    for (size_t i = 0; i < command->arg_count; i++) {
+        free(command->args[i]);
+    }
+}
+
+execute_command *factory(const Command *command) {
+    const char *name = command->name;
+    if (!strcmp(name, EXIT)) {
+        return &exit_command;
+    } else if (!strcmp(name, ECHO)) {
+        return &echo_command;
+    } else if (!strcmp(name, TYPE)) {
+        return &type_command;
+    }
+
+    return &unknown;
+}
+
+CommandType get_command_type(const Command *command) {
+    const char *name = command->name;
+    bool is_inbuilt = !strcmp(name, EXIT) || !strcmp(name, ECHO) || !strcmp(name, TYPE);
+
+    if (is_inbuilt) {
+        return INTERNAL;
+    }
+
+    return UNKNOWN;
+}
+
+CommandBehavior execute_generic_command(const Command *command) {
+    CommandType type = get_command_type(command);
+
+    switch (type) {
+        case INTERNAL:
+            return execute_inbuilt_command(command);
+        case EXTERNAL:
+            return EXECUTE;
+        case UNKNOWN:
+            return unknown(command);
+    }
+}
+
+CommandBehavior execute_inbuilt_command(const Command *command) {
+    execute_command *execute = factory(command);
+    return execute(command);
+}
+
+CommandBehavior unknown(const Command *command) {
+    printf("%s: command not found\n", command->name);
+    return EXECUTE;
+}
+
+CommandBehavior type_command(const Command *command) {
+    //there should be at least one argument
+    assert(command->arg_count >= 1);
+
+    char *type_argument = command->args[0];
+    Command type_command = parse_command(type_argument);
+    CommandType type = get_command_type(&type_command);
+
+    if (type == INTERNAL) {
+        printf("%s is a shell builtin\n", type_argument);
     } else {
-        not_found(command_name);
-        return CONTINUE;
+        printf("%s: not found\n", type_argument);
     }
+    return EXECUTE;
 }
 
-void echo_command(char *command) {
-    if (command == NULL) {
-        return;
-    }
-    char *arguments = ltrim(command + strlen(ECHO));
-    printf("%s\n", arguments);
+CommandBehavior exit_command(__attribute__((unused)) const Command *command) {
+    return TERMINATE;
 }
 
-void type_command(char *command) {
-    char *arguments = ltrim(command + strlen(ECHO));
-    char *true_name = get_command_type(arguments);
-
-    if (strcmp(true_name, NOT_FOUND) == 0) {
-        printf("%s: not found\n", arguments);
-        return;
-    }
-
-    if (strcmp(true_name, arguments) == 0) {
-        printf("%s is a shell builtin\n", true_name);
-        return;
-    }
-
-    printf("%s is %s\n", arguments, true_name);
-}
-
-void not_found(char *command_name) {
-    printf("%s: command not found\n", command_name);
-}
-
-int is_built_in(const char *command_name) {
-    return (!strcmp(command_name, ECHO) || !strcmp(command_name, EXIT) || !strcmp(command_name, TYPE));
-}
-
-char *is_external(const char *command_name) {
-    char *paths = get_path_variable();
-
-    int number_of_paths = get_number_of_paths(paths);
-    char *all_paths[number_of_paths];
-    segregate_paths(all_paths, paths);
-
-    return find_command(all_paths, number_of_paths, command_name);
-}
-
-char *get_path_variable() {
-    FILE *fp;
-    char path_value[BUFFER_SIZE];
-
-    if ((fp = popen(ECHO_PATH, "r")) != NULL) {
-        fgets(path_value, BUFFER_SIZE, fp);
-    }
-
-    return trim(path_value);
-}
-
-char *get_command_type(char *command_name) {
-    if (is_built_in(command_name)) {
-        return command_name;
-    }
-
-    char *absolute_path = is_external(command_name);
-    if (absolute_path != NULL) {
-        return absolute_path;
-    }
-
-    return NOT_FOUND;
-}
-
-int get_number_of_paths(const char *paths) {
-    int colon_count = 0;
-
-    while (*paths != '\0') {
-        if (*paths == ':') {
-            colon_count++;
-        }
-        paths++;
-    }
-
-    return colon_count + 1;
-}
-
-void segregate_paths(char **path_array, char *paths) {
-    int start_index = 0;
-    int path_index = 0;
-    for (int i = 0; i < strlen(paths); i++) {
-        if (paths[i] == ':') {
-            path_array[path_index] = strndup(paths + start_index, i - start_index);
-            path_index++;
-            start_index = i + 1;
-        }
-    }
-
-    path_array[path_index] = strndup(paths + start_index, strlen(paths) - start_index);
-}
-
-char *find_command(char **all_paths, int number_of_paths, const char *command_name) {
-    for (int i = 0; i < number_of_paths; i++) {
-        char *true_name = find_in_directory(all_paths[i], command_name);
-        if (strcmp(true_name, NOT_FOUND) == 0) {
-            continue;
-        }
-        return true_name;
-    }
-
-    return NOT_FOUND;
-}
-
-char *find_in_directory(char *directory_path, const char *command_name) {
-    DIR *dir = opendir(directory_path);
-
-    if (dir == NULL) {
-        return NOT_FOUND;
-    }
-
-    struct dirent *dir_pointer = NULL;
-
-    char *true_name = (char *) calloc(strlen(directory_path) + strlen(command_name) + 2, sizeof(char));
-    strcat(true_name, directory_path);
-    strcat(true_name, "/");
-    strcat(true_name, command_name);
-    while ((dir_pointer = readdir(dir)) != NULL) {
-        if (dir_pointer->d_type == DT_DIR) {
-            continue;
-        }
-        struct stat entry_info;
-        int status = stat(true_name, &entry_info);
-        if (status == 0) {
-            closedir(dir);
-            return true_name;
-        }
-    }
-
-    closedir(dir);
-    return NOT_FOUND;
+CommandBehavior echo_command(const Command *command) {
+    printf("%s\n", command->all_args);
+    return EXECUTE;
 }
